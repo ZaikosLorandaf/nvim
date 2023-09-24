770811cc75e1b970ecbf91d08f0163dd2b2eac6d
Benjamin Chausse <benjamin@chausse.xyz>
2023-09-24 04:48:34 -0400

    New fugitive commands

--- lua/ben/plugins/fugitive.lua
+++ lua/ben/plugins/fugitive.lua
@@ -5,9 +5,12 @@ return {
 		return vim.fn.isdirectory(".git") == 1
 	end,
 	config = function()
-		vim.keymap.set("n", "<leader>gs", ":Git<CR>")
-		vim.keymap.set("n", "<leader>gd", ":Git difftool<CR>")
+		vim.keymap.set("n", "<leader>gs", ":G<CR>")
+		vim.keymap.set("n", "<leader>gd", ":G difftool<CR>")
 		vim.keymap.set("n", "<leader>gm", ":Gvdiffsplit!<CR>")
-		vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")
+		vim.keymap.set("n", "<leader>gc", ":G commit<CR>")
+		vim.keymap.set("n", "<leader>gbb", ":GBrowse<CR>")
+		vim.keymap.set("n", "<leader>gps", ":G push<CR>")
+		vim.keymap.set("n", "<leader>gpl", ":G pull<CR>")
 	end,
 }
