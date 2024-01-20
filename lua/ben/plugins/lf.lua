17c22a2ec66af3c73834c035659eb2bc687f58c0
Benjamin Chausse <benjamin@chausse.xyz>
2024-01-20 04:29:44 -0500

    Simpler setup with lf as a file manager

--- lua/ben/plugins/lf.lua
+++ lua/ben/plugins/lf.lua
@@ -1,12 +1,15 @@
 return {
   "lmburns/lf.nvim",
+  lazy = false,
   dependencies = {
     { "akinsho/toggleterm.nvim", version = "*", config = true },
   },
-  config = {
-    border = "rounded",
-  },
-  keys = {
-    { "<leader>lf", "<cmd>Lf<cr>" },
-  },
+  config = function()
+    vim.g.lf_netrw = 1
+    require("lf").setup({
+      border = "rounded",
+      focus_on_open = true,
+    })
+    vim.api.nvim_set_keymap("n", "<leader>lf", "<cmd>Lf<cr>", { noremap = true, silent = true })
+  end,
 }
