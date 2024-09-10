4aae86537ac3ad986a52b4d67d2d4048829e61e9
zaikoslorandaf <sarah@gosselin.xyz>
2024-09-10 19:48:26 -0400

    add markdown and oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,34 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      columns = { "icon" },
+      keymaps = {
+        ["<C-h>"] = false,
+        ["<M-h>"] = "actions.select_split",
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    })
+  end,
+  keys = {
+
+    { -- Floating Oil window
+      "<leader>-",
+      function()
+        require("oil").toggle_float()
+      end,
+      mode = "n",
+      desc = "Toggle floating Oil window",
+    },
+
+    { -- Open parent directory
+      "-",
+      "<cmd>Oil<CR>",
+      mode = "n",
+      desc = "Open parent directory",
+    },
+  },
+}
