9c9efdd8bcb3809de7c3ecace1021839f8890aab
zaikoslorandaf <sarah@gosselin.xyz>
2024-10-10 17:19:03 -0400

    switch to rose-pine

--- init.lua
+++ init.lua
@@ -11,26 +11,29 @@ require("core.langmap")
 -- Bootstrap lazy.nvim {{{
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 -- }}}
+
 -- Configure Lazy {{{
 require("lazy").setup({
 	{ import = "plugins" }, -- i.e. the lua/plugins directory
 	-- add more subdirectories as needed
 }, {
-	install = { colorscheme = { "no-clown-fiesta" } },
+	-- install = { colorscheme = { "rose-pine" } },
+	-- install = { colorscheme = { "everforest" } },
+	-- install = { colorscheme = { "no-clown-fiesta" } },
 }, {
 	checker = {
 		enabled = true,
@@ -41,3 +44,4 @@ require("lazy").setup({
 	},
 })
 -- }}}
+vim.cmd.colorscheme("rose-pine")
